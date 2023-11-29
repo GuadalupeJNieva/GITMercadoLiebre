7c3ed2fc270395856346fe934473f28d472bc8fe
Guadalupe nieva <gjnuniversidad@gmail.com>
2023-11-29 15:52:55 -0300

    Trabajando con los formularios de las vistas y alguna animaciones

--- app.js
+++ app.js
@@ -11,3 +11,14 @@ app.get("/",function(req , res){
     res.sendFile(path.join(__dirname,"/views/home.html"))
 })
 
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get("/compras",(req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/compras.html'))
+})
